c5b477695db71574869e1cfc51353ba89fd22acf
Evgeniya Sheremetova <sei@symcmd.com>
2023-11-18 20:40:53 +0100

    Assign viewClass and layoutId
    These fields will help in cases of code refactoring not to forget about the associated tests screens

--- app/src/androidTest/kotlin/com/example/espressotest/screen/ListScreen.kt
+++ app/src/androidTest/kotlin/com/example/espressotest/screen/ListScreen.kt
@@ -2,6 +2,7 @@ package com.example.espressotest.screen
 
 import android.view.View
 import com.example.espressotest.R
+import com.example.espressotest.ui.list.ListActivity
 import com.kaspersky.kaspresso.screens.KScreen
 import io.github.kakaocup.kakao.image.KImageView
 import io.github.kakaocup.kakao.recycler.KRecyclerItem
@@ -10,8 +11,8 @@ import io.github.kakaocup.kakao.text.KTextView
 import org.hamcrest.Matcher
 
 object ListScreen : KScreen<ListScreen>() {
-    override val layoutId: Int? = null
-    override val viewClass: Class<*>? = null
+    override val layoutId: Int = R.layout.activity_list
+    override val viewClass: Class<*> = ListActivity::class.java
 
     val usersRecyclerView = KRecyclerView(
         builder = { withId(R.id.recyclerView) },
--- app/src/androidTest/kotlin/com/example/espressotest/screen/LoginScreen.kt
+++ app/src/androidTest/kotlin/com/example/espressotest/screen/LoginScreen.kt
@@ -1,13 +1,14 @@
 package com.example.espressotest.screen
 
 import com.example.espressotest.R
+import com.example.espressotest.ui.login.LoginActivity
 import com.kaspersky.kaspresso.screens.KScreen
 import io.github.kakaocup.kakao.edit.KEditText
 import io.github.kakaocup.kakao.text.KButton
 
 object LoginScreen : KScreen<LoginScreen>() {
-    override val layoutId: Int? = null
-    override val viewClass: Class<*>? = null
+    override val layoutId: Int = R.layout.activity_login
+    override val viewClass: Class<*> = LoginActivity::class.java
 
     val signInOrRegisterButton = KButton { withId(R.id.login) }
     val emailEditText = KEditText { withId(R.id.username) }
