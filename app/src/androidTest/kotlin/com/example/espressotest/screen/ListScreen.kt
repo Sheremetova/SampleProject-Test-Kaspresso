f228067954df8577ee0efc75a18b2fad80393b30
Evgeniya Sheremetova <sei@symcmd.com>
2023-11-18 13:43:58 +0100

    Optimize imports

--- app/src/androidTest/kotlin/com/example/espressotest/screen/ListScreen.kt
+++ app/src/androidTest/kotlin/com/example/espressotest/screen/ListScreen.kt
@@ -1,9 +1,9 @@
 package com.example.espressotest.screen
 
 import android.view.View
+import com.example.espressotest.R
 import com.kaspersky.kaspresso.screens.KScreen
 import io.github.kakaocup.kakao.image.KImageView
-import com.example.espressotest.R
 import io.github.kakaocup.kakao.recycler.KRecyclerItem
 import io.github.kakaocup.kakao.recycler.KRecyclerView
 import io.github.kakaocup.kakao.text.KTextView
